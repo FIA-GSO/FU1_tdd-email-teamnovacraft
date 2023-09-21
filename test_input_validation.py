7d7881f20a580045a2306c5eb784a20853f815f4
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-21 11:52:21 +0200

    reformat code

--- test_input_validation.py
+++ test_input_validation.py
@@ -1,37 +1,39 @@
 import pytest
+
 from input_validation import is_valid_email
 
+
 @pytest.mark.parametrize("email", [
-    ("test@email.com")
-,   ("t.est@email.com")
-,   ("test@em.ail.com")
-,   ("test@email.co.uk")
-,   ("te-st@email.com")
-,   ("te_st@email.com")
-,   ("test1@email.com")
+    "test@email.com",
+    "t.est@email.com",
+    "test@em.ail.com",
+    "test@email.co.uk",
+    "te-st@email.com",
+    "te_st@email.com",
+    "test1@email.com"
 ])
-def test_is_valid_email__gueltige_Adressen(email):
+def test_is_valid_email__gueltige_adressen(email):
     # arrange
     email_adress_to_be_tested = email
-    
+
     # act
     response = is_valid_email(email)
-    
+
     # assert
     assert response is True
 
 
 @pytest.mark.parametrize("email", [
-    ("testemail.com")   # Fehlendes @-Zeichen
-,   ("test@email")      # Fehlende Top-Level-Domain
-,   ("test@em@ail.com") # Mehrfaches @-Zeichen
+    "test email.com",  # Fehlendes @-Zeichen
+    "test@email",  # Fehlende Top-Level-Domain
+    "test@em@ail.com"  # Mehrfaches @-Zeichen
 ])
-def test_is_valid_email__ungueltige_Adressen(email):
+def test_is_valid_email__ungueltige_adressen(email):
     # arrange
     email_adress_to_be_tested = email
-    
+
     # act
     response = is_valid_email(email)
-    
+
     # assert
     assert response is False
