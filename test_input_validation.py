afc7726ac7987d404e69df07ce66a6b04d88862d
Hakan Kök <hakan.k10@gso.schule.koeln>
2023-09-21 12:47:17 +0200

    Add additional test cases

--- test_input_validation.py
+++ test_input_validation.py
@@ -11,6 +11,17 @@ from input_validation import is_valid_email
     "te-st@email.com",
     "te_st@email.com",
     "test1@email.com",
+    "test+text@email.com",
+    "test@123.123.123.123",
+    "test@[123.123.123.123]",
+    "\"test\"@email.com",
+    "1234567890@email.com",
+    "test@email-domain.com",
+    "____@email.com",
+    "test.\"text\\why\"@email.com",
+    "why.must.\"@\".we.suffer@example.com",
+    "very.\"(),:;<>[]\".VERY.\"very@\\\\\\", # do not question it
+    "\\\"very\".unusual@strange.example.com"
     "test+other@email.com"
 ])
 def test_is_valid_email__valid_addresses(email):
@@ -22,9 +33,10 @@ def test_is_valid_email__valid_addresses(email):
 
 
 @pytest.mark.parametrize("email", [
-    "test email.com",
-    "test@email",
-    "test@em@ail.com"
+    "test email.com",   # Fehlendes @-Zeichen
+    "test@email",       # Fehlende Top-Level-Domain
+    "test@em@ail.com",  # Mehrfaches @-Zeichen
+    "test@email.",      # Fehlende Top-Level-Domain
 ])
 def test_is_valid_email__invalid_addresses(email):
     # act
